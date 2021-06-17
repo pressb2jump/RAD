512b97f59dde52eed0dec9949007b2fc555bce82
Kyle-Chamberlain <Kyle.chamberlain15@gmail.com>
2021-06-17 10:20:26 +0800

    new pages
    adding new pages to the website file

--- WebSite/top_Rated.php
+++ WebSite/top_Rated.php
@@ -31,41 +31,12 @@ require 'header.php';
 $db = DB_connect();
 // declare arrays that will be used below
 $results = array();
-$current_results = array();
-$last_results = array();
-// Uses function from database to get top 10 rated movies
-$current = Current_Top_rated();
-$last = Compare_Last_rated();
-$all_top_rated = All_Top_rated();
-//gets current top 10 rated from movies table
-while ($currentID = mysqli_fetch_assoc($current)) {
-    $current_results[] = $currentID['ID'];
-}
-//print_r($current_results);
-//gets last row of rating table
-while ($lastrow = mysqli_fetch_assoc($last)) {
-    $last_results = array($lastrow['1st']
-    , $lastrow['2nd']
-    , $lastrow['3rd']
-    , $lastrow['4th']
-    , $lastrow['5th']
-    , $lastrow['6th']
-    , $lastrow['7th']
-    , $lastrow['8th']
-    , $lastrow['9th']
-    , $lastrow['10th']);
-}
-//print_r($last_results);
-//compare 2 arrays and do an update if different
-if (array_diff($current_results,$last_results) != null) //different
-{
-    //print_r(array_diff($current_results,$last_results));
-    Insert_New_top10($current_results);
-    //echo "Updated";
-}
-else //same
-{
-    //echo "same";
+// Uses function from database to get top 10 movies
+$movies_set = Find_Top_movies();
+// returns the array as movie_set objects
+while ($movies = mysqli_fetch_assoc($movies_set)) {
+        // add to array with Title as index(Key) and Search_Hits as Value 
+        $results[] = array("label" => $movies['Title'], "y" => $movies['Search_Hits']);
 }
 ?>
 <!DOCTYPE html>
@@ -79,8 +50,26 @@ else //same
     <link rel="stylesheet" 
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="StyleSheet.css" />
-    <title>Top 10 Rated</title>
-     
+    <title>Top 10</title>
+    <script>
+    window.onload = function () {
+ 
+    var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title: {
+		text: "Top 10 Searched Movies"
+	},
+	axisY: {
+		title: "Number of Searches"
+	},
+	data: [{
+		type: "column",
+		dataPoints: <?php echo json_encode($results, JSON_NUMERIC_CHECK); ?>
+	}]
+    });
+    chart.render(); }
+    </script> 
 </head>
 <body>
 <div id="data-body" class="row">
@@ -89,90 +78,14 @@ else //same
         <ul class="nav nav-pills nav-stacked">
             <li><a href="search_Movie.php">Search Movies</a></li>
             <li><a href="top_Searched.php">Top 10 Searched Movies</a></li>
-            <li><a href="top_Rated.php">Top 10 Rated Movies</a></li>
             <li><a href="userSignUp.php">User Signup</a></li>
             <li><a href="showUsers.php">View All Users</a></li>
             <li><a href="adminLogIn.php">Administrator Section</a></li>
         </ul> 
-    </nav>  
+    </nav>
+      
     <main class="col-lg-20">
-        <h1>Top 10 Rated Movies</h1>
-        <form action="<?php echo htmlspecialchars(
-            $_SERVER["PHP_SELF"]
-        ); 
-?>" method="post">
-        <div class="form-group">
-                <label for="selection">Date of Ratings:</label>
-                <select onChange= "this.form.submit()" name="selection" id="selection">
-                <option  <?php ?>>
-                <?php
-                while ($row = mysqli_fetch_array($all_top_rated)) {
-                    echo("<option value='".$row['rating_id']."|".$row['date']."'>
-                    ".$row['date']." </option>");
-                }?>
-                </select>
-                <script type="text/javascript">
-                    document.getElementById('selection').value = "<?php echo $_GET['selection'];?>";
-                </script>
-            </div>
-        </form>
-        <?php
-        if (isset($_POST['selection']))
-        {
-            $value = $_POST['selection'];
-            //echo $value;
-            $value_explode = explode('|', $value);
-            $id = $value_explode[0];
-            $date = $value_explode[1];
-            //echo $id;
-            getResults($id, $date);
-        }
-        function getResults($id, $date)
-        {
-            $row = Top_Rated_info($id);
-            // unset($results);
-            // $results = array();
-            while ($movies = mysqli_fetch_assoc($row)) {
-                // add to array with Title as index(Key) and Search_Hits as Value 
-                $results[] = array("label" => $movies['Title'], "y" => $movies['Rating_Average']);
-            }
-            //print_r($results);
-            build_chart($results, $date);
-        }
-
-        function build_chart($results, $date)
-        {?>
-        <script>
-        window.onload = function () {
-        var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        theme: "light2", // "light1", "light2", "dark1", "dark2"
-        title: {
-            text: "Top 10 rated movies"
-        },
-        subtitles:[
-		{
-			text: "<?php echo $date; ?>"
-			//Uncomment properties below to see how they behave
-			//fontColor: "red",
-			//fontSize: 30
-		}
-		],
-        axisY: {
-            title: "Rating Average"
-        },
-        data: [{
-            //legendMarkerType: "square",
-            type: "column",
-            //showInLegend: true,
-            dataPoints:<?php echo json_encode($results, JSON_NUMERIC_CHECK); ?>
-            
-        }]
-        });
-        chart.render(); }
-        </script>
-        <?php }
-    ?>
+        <h1>Top 10 most Searched Movies</h1>
         <div id="chartContainer" style="height: 370px; width: 80%;"></div>
         <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     </div>
